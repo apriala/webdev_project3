0b9f81dd57bf6a9e18a2c9e36b8b733e4607f071
apriala <apriala@hawk.iit.edu>
2024-11-12 23:06:55 -0600

    configure map and set up markers

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
   function initMap(){
   var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 41.8729, lng: -87.62793},
+      center: {lat: 41.8312, lng: -87.6272},
       zoom: 12
+    });
+	const map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    const infoWindow = new google.maps.InfoWindow({
+      content: "<p>infoWindow</p>"
+    });
+    marker.addListener("click", function() {
+      infoWindow.open(map, marker);
     });
 }
